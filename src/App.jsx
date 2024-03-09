90842b6d25eb621e735277a7724ea1ef03234aa1
Daffa Muttaqin <nine@upi.edu>
2024-03-09 15:15:47 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import Modal from "./components/Modal";
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Modal() {
+  return <div>Testing...</div>;
+}
+
+export default Modal;

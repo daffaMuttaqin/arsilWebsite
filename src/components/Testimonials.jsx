a75b3b62095037ad58a17a5f893113c84e9925a0
daffaMuttaqin <daffamuttaqinwork@gmail.com>
2025-10-07 16:14:26 +0700

    Mengganti testimoni dari data percobaan menjadi API

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,21 +1,40 @@
 import { motion } from "framer-motion";
 import Section from "./ui/Section";
 import { fadeInUp } from "../utils/animations";
-import { TESTIMONIALS } from "../data/testimonials";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Testimonials() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/api/testimonials`
+        );
+
+        setData(res.data);
+      } catch (error) {
+        setErr(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Section id="testimonials" title="Apa kata klien" subtitle="Testimoni">
       <motion.div {...fadeInUp} className="grid md:grid-cols-3 gap-5">
-        {TESTIMONIALS.map((t) => (
-          <figure key={t.name} className="rounded-2xl border p-5 bg-white">
+        {data.map((item) => (
+          <figure key={item.id} className="rounded-2xl border p-5 bg-white">
             <blockquote className="text-sm leading-relaxed text-neutral-700">
-              “{t.quote}”
+              “{item.quote}”
             </blockquote>
             <figcaption className="mt-3 text-sm font-medium">
-              {t.name}
+              {item.name}
             </figcaption>
-            <div className="text-xs text-neutral-500">{t.role}</div>
+            <div className="text-xs text-neutral-500">{item.role}</div>
           </figure>
         ))}
       </motion.div>

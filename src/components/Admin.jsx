4a5b527d7d37fd5b09fcb8fb4eb1f3c547966f64
daffaMuttaqin <daffamuttaqinwork@gmail.com>
2025-10-18 16:53:26 +0700

    Fix bug

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -98,7 +98,7 @@ function Admin() {
         for (let pair of formData.entries()) {
           console.log(pair[0], pair[1]);
         }
-        res = await API.put(`/projects/${editingProject.id}`, formData, {
+        res = await API.patch(`/projects/${editingProject.id}`, formData, {
           headers: { "Content-Type": "multipart/form-data" },
         });
 
@@ -557,7 +557,6 @@ function Admin() {
 
               {/* Tombol Submit */}
               <button
-                type="submit"
                 onClick={handleSubmit}
                 className="mt-4 text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
               >
